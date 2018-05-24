05ce70f3c751b55ab62fe4d036c0649785b568a4
Arvind <arvind01.aipl@gmail.com>
2018-05-24 17:38:09 +0530

    ff

--- functions.php
+++ functions.php
@@ -7,7 +7,11 @@ echo "Functions3 by master";
 echo "Functions3 by branch2";
 echo "Functions3 by branch3";
 echo "Functions4 by master";
+<<<<<<< Updated upstream
 echo "Functions8 by master";
 echo "Functions9 by master";
 echo "Functions11 by master";
+=======
+echo "Functions5 by master";
+>>>>>>> Stashed changes
 ?>
